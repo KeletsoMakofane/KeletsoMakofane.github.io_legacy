d8f4d4467833c00c124880658b992372e0f7e4d9
Keletso Makofane <kmakofane@g.harvard.edu>
2020-09-12 19:59:51 -0400

    commit

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -109,8 +109,8 @@ const ArchivePage = ({ location, data }) => {
   return (
     <Layout location={location}>
       <Helmet>
-        <title>Archive | Brittany Chiang</title>
-        <link rel="canonical" href="https://brittanychiang.com/archive" />
+        <title>Archive | Keletso Makofane</title>
+        <link rel="canonical" href="https://keletsomakofane.com/archive" />
       </Helmet>
 
       <StyledMainContainer>
