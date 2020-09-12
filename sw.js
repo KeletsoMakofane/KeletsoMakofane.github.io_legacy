9c209fc679ac782dfff75fc82912d591212a091d
Keletso Makofane <kmakofane@g.harvard.edu>
2020-09-12 19:42:37 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2d7cf8111392a06d356c.js"
+    "url": "webpack-runtime-6dc3a296dcc9bf2dfdf3.js"
   },
   {
     "url": "framework-85aff51d15c28c7fbe5f.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "081a3bbc6eca52834c05b20ff3a09ebb"
+    "revision": "47d8a989a8b5b6e0291cb1d494596578"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7c31e2436cade51cbcda.js"
